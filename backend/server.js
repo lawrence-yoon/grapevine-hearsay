8619d5a4f7477a35de6565122ee8a3c10380ee70
sadpandaemoji <sadpandaemoji@gmail.com>
2022-10-11 10:18:56 -0400

    prepare for production and deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require('colors')
@@ -15,6 +16,14 @@ app.use(express.urlencoded({extended: false}))
 
 app.use("/api/secrets", require('./routes/secretsRoutes'))
 app.use("/api/users", require('./routes/userRoutes'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}   else{
+    app.get('/', (req, res) => res.send('Please set to NODE_ENV to production'))
+}
+
 app.use(errorHandler)
 
 app.listen(port,()=>console.log(`server started on port ${port}`))
