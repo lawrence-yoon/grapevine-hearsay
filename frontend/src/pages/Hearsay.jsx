19f6946bd3d8286711dd7eab4d6908b72b534712
sadpandaemoji <sadpandaemoji@gmail.com>
2022-10-11 14:18:46 -0400

    update style and reverse hearsay map

--- frontend/src/pages/Hearsay.jsx
+++ frontend/src/pages/Hearsay.jsx
@@ -28,7 +28,6 @@ function Hearsay() {
     <h1>This is hearsay.</h1>
     <p>Do not believe everything you read on the internet.</p>
   </section>
-  {/* I need to get a way to fetch all the secrets, and render them here. It is a little foggy in my head because its obfuscated by auth. I suppose I need to get good at that stuff. */}
   <section className="content">
     {/* {secrets.length>0?
       <div className='secrets'>
@@ -39,7 +38,7 @@ function Hearsay() {
     :<h3>You have no secrets</h3>} */}
     {hearsay.map((secret)=>(
           <HearsayItem key={secret._id} secret={secret} />
-    ))}
+    )).reverse()}
   </section>
 </>
 }
